ad5b321b997591d76fb960a00db6b1240d40a329
Noen <noen@noendex.com>
2017-05-16 11:32:41 +0200

    Description fixes

--- PSScripts/info.bat
+++ PSScripts/info.bat
@@ -1,8 +1,8 @@
 @echo off
 
-cd "C:\SLApplications\NG2ServerStatus"
+cd "C:\NG2ServerStatus"
 
-del /f /q C:\SLApplications\NG2ServerStatus\assets\servers.json
-del /f /q C:\SLApplications\NG2ServerStatus\assets\ping.json
+del /f /q C:\NG2ServerStatus\assets\servers.json
+del /f /q C:\NG2ServerStatus\assets\ping.json
 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\SLApplications\NG2ServerStatus\info.ps1" -ExecutionPolicy Unrestricted
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\NG2ServerStatus\info.ps1" -ExecutionPolicy Unrestricted
